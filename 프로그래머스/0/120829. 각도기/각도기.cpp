00d42cdfeb0011eb913bd58c473d2010f1597805
Jiyoon Park <jiyoonpark.dev@gmail.com>
2024-08-23 12:13:45 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    
+    if (angle == 90)
+    {
+        return 2;
+    }
+    else if (angle == 180)
+    {
+        return 4;
+    }
+    else if (angle < 90)
+    {
+        return 1;
+    }
+    else if (angle > 90)
+    {
+        return 3;
+    }
+}
