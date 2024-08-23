65f288212023bf7579175920c24cc5d69315f10b
Jiyoon Park <jiyoonpark.dev@gmail.com>
2024-08-23 12:20:36 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int sum = 0;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
