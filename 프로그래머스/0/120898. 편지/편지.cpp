e8cac53a41c3c2cd2c349a15f74dbd36c21b2f5e
Jiyoon Park <jiyoonpark.dev@gmail.com>
2024-09-05 01:09:35 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string message) {
+    int answer = 0;
+    int len = message.length();
+    
+    answer = len * 2;
+    
+    return answer;
+}
