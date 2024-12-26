38e18ae095436400c57007b15de44db595e87ce4
Jiyoon Park <jiyoonpark.dev@gmail.com>
2024-12-26 10:34:25 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -6,15 +6,9 @@ using namespace std;
 
 int solution(vector<int> numbers) {
     int answer = 0;
-    int size = numbers.size();
-    int num1 = 0;
-    int num2 = 0;
     
     sort(numbers.begin(), numbers.end());
-    num1 = numbers[size - 1];
-    num2 = numbers[size - 2];
-    
-    answer = num1 * num2;
+    answer = numbers[numbers.size() - 1] * numbers[numbers.size() - 2];
     
     return answer;
 }
