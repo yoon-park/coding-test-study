35e29a4f64c60f073a22d0a541192f3365ad4491
Jiyoon Park <jiyoonpark.dev@gmail.com>
2024-12-27 09:50:00 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    int size = signs.size();
+    
+    for (int i = 0; i < size; i++)
+    {
+        if (signs[i] == true)
+        {
+            answer += absolutes[i];
+        }
+        else
+        {
+            answer += absolutes[i] * (-1);
+        }
+    }
+    
+    return answer;
+}
